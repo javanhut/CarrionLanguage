b052fdb79803c4965e39d78993bcf47251acffb0
javanhut <javan.hutchinson18@gmail.com>
2025-04-20 12:47:20 -0400

    feat: added more to lexer

--- src/lexer/mod.rs
+++ src/lexer/mod.rs
@@ -1,4 +1,5 @@
 use std::collections::HashMap;
+use std::path::PathBuf;
 use token::Token;
 
 pub struct Lexer {
@@ -7,6 +8,7 @@ pub struct Lexer {
     start: usize,
     current: usize,
     line: usize,
+    file: PathBuf,
 }
 
 impl Lexer {
@@ -23,6 +25,7 @@ impl Lexer {
             start: 0,
             current: 0,
             line: 1,
+            file,
         }
     }
 
@@ -35,12 +38,104 @@ impl Lexer {
         std::mem::take(&mut self.tokens)
     }
     fn scan_token(&mut self) {
-        let c = self.advance();
+        let Some(c) = self.advance() else {
+            return;
+        };
         match c {
             '(' => self.add_token(TokenType::LeftParen),
             ')' => self.add_token(TokenType::RightParen),
             '[' => self.add_token(TokenType::LeftBracket),
             ']' => self.add_token(TokenType::RightBracket),
+            '{' => self.add_token(TokenType::LeftBrace),
+            '}' => self.add_token(TokenType::RightBrace),
+            '@' => self.add_token(TokenType::At),
+            '%' => self.add_token(TokenType::Mod),
+            '/' => {
+                if self.match_char('/') {
+                    while self.peek() != Some('\n') && !self.is_at_end() {
+                        self.advance();
+                    } else if self.match_char('*') {
+                        self.block_comment();
+                    } else if self.match_char('=') {
+                        self.add_token(TokenType::SlashAssign);
+                    } else {
+                        self.add_token(TokenType::Slash);
+                    }
+                }
+            }
+            '!' => {
+                let t = if self.match_char('=') {
+                    TokenType::NotEqual
+                } else {
+                    TokenType::Not
+                };
+                self.add_token(t);
+            }
+            '*' => {
+                let t = if self.match_char('=') {
+                    TokenType::AsteriskAssign
+                } else if self.match_char('*'){
+                    TokenType::Exponent
+                }
+                else {
+                    TokenType::Asterisk
+                };
+                self.add_token(t);
+            }
+            '>' => {
+                let t = if self.match_char('=') {
+                    TokenType::GreaterThanEqual
+                } else if self.match_char('>') {
+                    TokenType::RightShift
+                } else {
+                    TokenType::GreaterThan
+                };
+                self.add_token(t);
+            }
+            '<' => {
+                let t = if self.match_char('=') {
+                    TokenType::LessThanEqual
+                } else if self.match_char('<') {
+                    TokenType::LeftShift
+                } else {
+                    TokenType::LessThan
+                };
+                self.add_token(t);
+            }
+            '=' => {
+                let t = if self.match_char('=') {
+                    TokenType::Equality
+                } else {
+                    TokenType::Assign
+                };
+                self.add_token(t)
+            }
+            '+' => {
+                let t = if self.match_char('+') {
+                    TokenType::PlusPlusIncrement
+                } else if self.match_char('=') {
+                    TokenType::PlusAssign
+                } else {
+                    TokenType::Plus
+                };
+                self.add_token(t);
+            }
+
+            '-' => {
+                let t = if self.match_char('-') {
+                    TokenType::MinusMinusDecrement
+                } else if self.match_char('=') {
+                    TokenType::MinusAssign
+                } else {
+                    TokenType::Minus
+                };
+                self.add_token(t);
+            }
+            ' '| '\r' | '\t' => {}
+            '\n' => {
+                self.line += 1;
+            }
+
 
             _ => self.add_token(TokenType::Illegal),
         }
@@ -48,27 +143,39 @@ impl Lexer {
     fn is_at_end(&self) -> bool {
         self.current >= self.source.len()
     }
-    fn advance(&mut self) -> char {
-        let ch = self.source.as_byte()[self.current] as char;
-        self.current += 1;
-        ch
+    fn advance(&mut self) -> Option<char> {
+        if self.is_at_end() {
+            return None;
+        }
+        let ch = self.source[self.current..].chars().next()?;
+        let char_len = ch.len_utf8();
+        self.current += char_len;
+        Some(ch)
     }
 
     fn peek(&self) -> Option<char> {
-        self.source.chars().nth(self.current)
+        if self.is_at_end() {
+            return None;
+        }
+        self.source[self.current..].chars().next()
     }
     fn peek_next(&self) -> Option<char> {
-        self.source.chars().nth(self.current + 1)
+        if self.is_at_end() {
+            return None;
+        }
+        let mut chars = self.source[self.current..].chars();
+        chars.next();
+        chars.next()
     }
     fn match_char(&mut self, expected: char) -> bool {
         if self.peek() == Some(expected) {
-            self.current += 1;
+            self.advance();
             true
         } else {
             false
         }
     }
-    fn add_token(&mut self, ttype: TokenType) {
+    fn add_token(&mut self, ttype: TokenType, literal: Option<LiteralValue>) {
         let text = &self.source[self.start..self.current];
         self.tokens.push(Token::new(
             ttype,
